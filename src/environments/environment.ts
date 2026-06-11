29c0bb64a0eaee25849c76088fe636171f1e5ab2
eoschwald <eoschwald@adobe.com>
2026-06-11 23:38:11 +0200

    Add environment configuration and project assets

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+import { AppEnvironment } from '../app/app.tokens';
+
+export const environment: AppEnvironment = {
+  apiBaseUrl: 'http://localhost:8081/api',
+
+  auth: {
+    issuer: 'http://localhost:8080/realms/fitness-tracker',
+
+    clientId: 'fitness-tracker-client',
+
+    redirectUri: 'http://localhost:4200',
+
+    postLogoutRedirectUri: 'http://localhost:4200',
+
+    scope: 'openid profile email'
+  }
+};
